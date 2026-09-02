a3d84000aa014ae92b3305ee895576efcb0eb736
DejanDj79 <deki4679@gmail.com>
2026-09-02 20:36:48 +0200

    Remove Dashboard v2 layout wrapper

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,5 @@
 import { Dashboard } from "@/components/dashboard/Dashboard";
-import styles from "./DashboardV2Layout.module.css";
 
 export default function DashboardPage() {
-  return (
-    <div className={styles.dashboardV2}>
-      <Dashboard />
-    </div>
-  );
+  return <Dashboard />;
 }
