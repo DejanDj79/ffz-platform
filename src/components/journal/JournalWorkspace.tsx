4e19303a044a1f2085612a8ad10cfe689f495bcb
DejanDj79 <deki4679@gmail.com>
2026-09-03 00:58:39 +0200

    Connect planned trades with Journal

---
+++ src/components/journal/JournalWorkspace.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useState } from "react";
+import { PlannedTradesPanel } from "./PlannedTradesPanel";
+import { TradeJournal } from "./TradeJournal";
+
+export function JournalWorkspace() {
+  const [journalVersion, setJournalVersion] = useState(0);
+
+  return (
+    <>
+      <PlannedTradesPanel
+        onTradeStarted={() => setJournalVersion((current) => current + 1)}
+      />
+      <TradeJournal key={journalVersion} />
+    </>
+  );
+}
