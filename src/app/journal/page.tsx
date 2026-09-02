961921ad6d4de9da4bf6d5f80bb70aba73916c5f
DejanDj79 <deki4679@gmail.com>
2026-09-02 20:58:25 +0200

    Remove in-content journal tabs

--- src/app/journal/page.tsx
+++ src/app/journal/page.tsx
@@ -1,11 +1,5 @@
-import { JournalNav } from "@/components/journal/JournalNav";
 import { TradeJournal } from "@/components/journal/TradeJournal";
 
 export default function JournalPage() {
-  return (
-    <>
-      <JournalNav active="TRADES" />
-      <TradeJournal />
-    </>
-  );
+  return <TradeJournal />;
 }
