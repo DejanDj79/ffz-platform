f16617d7f6e924c7bfc220cbee288f284e8dab62
DejanDj79 <deki4679@gmail.com>
2026-09-02 20:58:46 +0200

    Use sidebar navigation for journal analytics

--- src/app/journal/analytics/page.tsx
+++ src/app/journal/analytics/page.tsx
@@ -1,11 +1,5 @@
 import { JournalAnalytics } from "@/components/journal/JournalAnalytics";
-import { JournalNav } from "@/components/journal/JournalNav";
 
 export default function JournalAnalyticsPage() {
-  return (
-    <>
-      <JournalNav active="ANALYTICS" />
-      <JournalAnalytics />
-    </>
-  );
+  return <JournalAnalytics />;
 }
