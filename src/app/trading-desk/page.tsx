cb9eca2dafc7fa6eef1e7aa042f09b6e48df01bd
DejanDj79 <deki4679@gmail.com>
2026-09-05 21:01:09 +0200

    Reposition weekly focus inside Trading Desk

--- src/app/trading-desk/page.tsx
+++ src/app/trading-desk/page.tsx
@@ -1,11 +1,5 @@
 import { DailyTradingDeskFunded } from "@/components/trading-desk/DailyTradingDeskFunded";
-import { WeeklyFocusReminder } from "@/components/trading-desk/WeeklyFocusReminder";
 
 export default function TradingDeskPage() {
-  return (
-    <>
-      <WeeklyFocusReminder />
-      <DailyTradingDeskFunded />
-    </>
-  );
+  return <DailyTradingDeskFunded />;
 }
