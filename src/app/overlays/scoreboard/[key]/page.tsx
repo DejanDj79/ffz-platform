66b04b36fd1f23c040aeb4407931661f0f05eef2
DejanDj79 <deki4679@gmail.com>
2026-09-08 10:11:21 +0200

    Increase scoreboard performance and notes text

--- src/app/overlays/scoreboard/[key]/page.tsx
+++ src/app/overlays/scoreboard/[key]/page.tsx
@@ -12,6 +12,15 @@ export default async function ScoreboardOverlayPage({
   return (
     <div className="scoreboard-overlay-root">
       <ScoreboardOverlay overlayKey={key} />
+      <style>{`
+        .scoreboard-overlay-root > div > section > section:nth-of-type(2) > article:nth-child(2) > div > div > span {
+          font-size: calc(clamp(6px, .55vw, 10px) + 3px) !important;
+        }
+
+        .scoreboard-overlay-root > div > section > section:nth-of-type(3) > article:nth-child(2) > div:first-of-type > div > span {
+          font-size: calc(clamp(6px, .58vw, 10px) + 3px) !important;
+        }
+      `}</style>
     </div>
   );
 }
