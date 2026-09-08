f41fe609ac18d20c1d94b9696c1343e0cbe13b61
DejanDj79 <deki4679@gmail.com>
2026-09-08 13:24:32 +0200

    Add Creator publish fields

---
+++ drizzle/0008_creator_publish_fields.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "creator_episodes" ADD COLUMN "publish_title" varchar(180);
+ALTER TABLE "creator_episodes" ADD COLUMN "thumbnail_text" varchar(120);
+ALTER TABLE "creator_episodes" ADD COLUMN "description" text;
+ALTER TABLE "creator_episodes" ADD COLUMN "chapters" text;
+ALTER TABLE "creator_episodes" ADD COLUMN "youtube_url" varchar(500);
